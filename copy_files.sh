44c84500ccd4284d75778fa8b7403b0e65a03b5c
Thong Thanh Nguyen <thongnguyen050999@gmail.com>
2024-08-31 15:38:37 +0800

    Update copy_files.sh

--- copy_files.sh
+++ copy_files.sh
@@ -1,16 +1,18 @@
-cp -r /mnt/hdd/Thong/malmm_project/MA-LMM/app ./
-ln -s /mnt/hdd/Thong/malmm_project/MA-LMM/data ./
-ln -s /mnt/hdd/Thong/malmm_project/MA-LMM/llm ./
+cp -r ../MA-LMM/app ./
+ln -s ../MA-LMM/data ./
+ln -s ../MA-LMM/llm ./
 mkdir lavis
-cp -r /mnt/hdd/Thong/malmm_project/MA-LMM/lavis/common ./
-cp -r /mnt/hdd/Thong/malmm_project/MA-LMM/lavis/configs ./
-cp -r /mnt/hdd/Thong/malmm_project/MA-LMM/lavis/datasets ./
-cp -r /mnt/hdd/Thong/malmm_project/MA-LMM/lavis/models ./
-cp -r /mnt/hdd/Thong/malmm_project/MA-LMM/lavis/processors ./
-cp -r /mnt/hdd/Thong/malmm_project/MA-LMM/lavis/projects ./
-cp -r /mnt/hdd/Thong/malmm_project/MA-LMM/lavis/runners ./
-cp -r /mnt/hdd/Thong/malmm_project/MA-LMM/lavis/tasks ./
-cp /mnt/hdd/Thong/malmm_project/MA-LMM/lavis/*py ./
-cp -r /mnt/hdd/Thong/malmm_project/MA-LMM/run_scripts ./
-cp -r /mnt/hdd/Thong/malmm_project/MA-LMM/tests ./
-cp /mnt/hdd/Thong/malmm_project/MA-LMM/*py ./
+cd lavis
+cp -r ../../MA-LMM/lavis/common ./
+cp -r ../../MA-LMM/lavis/configs ./
+cp -r ../../MA-LMM/lavis/datasets ./
+cp -r ../../MA-LMM/lavis/models ./
+cp -r ../../MA-LMM/lavis/processors ./
+cp -r ../../MA-LMM/lavis/projects ./
+cp -r ../../MA-LMM/lavis/runners ./
+cp -r ../../MA-LMM/lavis/tasks ./
+cp ../../MA-LMM/lavis/*py ./
+cd ..
+cp -r ../MA-LMM/run_scripts ./
+cp -r ../MA-LMM/tests ./
+cp ../MA-LMM/*py ./
